977084bed230a5358ecf7a2ba5af818f265a5cbd
Rukundojoseph <104127964+Rukundojoseph@users.noreply.github.com>
2023-01-04 21:13:08 -0900

    feat: finished the whole intergration

---
+++ index.js
@@ -0,0 +1,41 @@
+function setCookie(name, value, expirationDays) {
+  const date = new Date();
+  date.setTime(date.getTime() + (expirationDays * 1));
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${name}=${value};${expires};path=/`;
+}
+
+function getCookie(name) {
+  const nameEQ = `${name}=`;
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
+}
+
+function signout(){
+  setCookie("token","" , 1 )
+} 
+window.addEventListener("load",()=>{
+  var acc=document.querySelector("#account")
+  if( getCookie("token")==null){
+    acc.innerHTML=`LOGIN`
+    acc.href="./signin/signin.html"    
+  }
+  else{
+    acc.innerHTML=`LOGOUT`
+    acc.addEventListener('click',()=>{   
+    signout()        
+    acc.href="./signin/signin.html"    
+    acc.innerHTML=`LOGIN`    
+    })    
+  }
+})
+
